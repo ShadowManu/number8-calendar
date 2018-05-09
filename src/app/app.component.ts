6c627c51fb83b5e9136170a857881bf92ffeff37
Manuel Pacheco <manuelalejandropm@gmail.com>
2018-05-09 11:54:26 -0400

    implement core calendar construction, update, and styling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnChanges, SimpleChanges } from '@angular/core';
-import { range } from 'lodash';
+import { Component } from '@angular/core';
+import { range, set } from 'lodash';
+import { DateTime } from 'luxon';
 
 interface CalendarData { [year: number]: YearData; }
 interface YearData { [month: number]: MonthData; }
@@ -7,44 +8,70 @@ interface MonthData { [day: number]: DayData; }
 
 interface DayData {
   text: string;
-  color: string;
+  css: string;
 }
 
 const DEFAULT_DAY_DATA = {
   text: '',
-  color: 'grey'
+  css: 'empty'
 };
+
+function buildCalendar(start, days): CalendarData {
+  const startTime = DateTime.fromFormat(start, 'L/d/y');
+
+  if (!startTime.isValid) { return {}; }
+
+  const calendar = {};
+
+  for (let i = 0; i < days; i++) {
+    const dayTime = startTime.plus({ days: i });
+    const { year, month, day } = dayTime.toObject();
+
+    const isWeekend = [6, 7].includes(dayTime.weekday);
+    const css = isWeekend ? 'weekend' : 'normal';
+
+    const dayObj = { text: day, css };
+    set(calendar, [year, month, day], dayObj);
+  }
+
+  return calendar;
+}
 
 @Component({
   selector: 'n8c-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnChanges {
-  start: string; // Date string
-  days: number;
+export class AppComponent {
+  start = '05/09/2018'; // Date string
+  days = 15;
   code: string;
 
-  calendar: CalendarData = {
-    2018: {
-      5: {
-        9: {
-          text: '9!',
-          color: 'blue'
-        }
-      }
-    }
-  };
+  calendar: CalendarData;
 
-  ngOnChanges(changes: SimpleChanges) {
-    // updateCalendarStructure
+  constructor() {
+    this.updateCalendar();
   }
+
+  changeStart(start: string) {
+    this.start = start;
+    this.updateCalendar();
+  }
+
+  changeDays(days: number) {
+    this.days = days;
+    this.updateCalendar();
+  }
+
+  updateCalendar() { this.calendar = buildCalendar(this.start, this.days); }
 
   keys<T extends Object>(obj: T) { return Object.keys(obj); }
 
   tabulate(year: number, month: number, data: MonthData) {
+    const startTime = DateTime.fromObject({ year, month });
+    const offset = startTime.weekday % 7;
+    // TODO Define rows
     const rows = 5;
-    const offset = 0;
 
     return range(rows).map(row => {
       return range(7).map(column => {
